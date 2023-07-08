16a9281d42c4b1e4855cc57234634b5fd442cee0
samuel1509 <samuel-1509@hotmail.com>
2023-07-07 21:22:04 -0300

    Actualizado rep de ejercicios

---
+++ Clase32POO/apppersona/src/main/java/com/cma/Application.java
@@ -0,0 +1,25 @@
+package com.cma;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Persona p1 = new Persona();
+        p1.setEdad(-1);// probamos valicdacion de edad >0
+        System.out.println("La edad de la persona es: " + p1.getEdad());
+        p1.setEdad(30);
+        System.out.println("La edad de la persona es: " + p1.getEdad());
+        p1.setNombre("Susana");
+        p1.mensaje();
+        System.out.println("Personas generadas: " + Persona.getCantPersona());
+        Persona p2 = new Persona("Cristian", 25);
+
+        p2.mensaje();
+
+        System.out.println("Personas generadas: " + Persona.getCantPersona());
+
+        Persona p3 = new Persona("Pepe", 50);
+        p3.mensaje();
+        System.out.println("Personas generadas: " + Persona.getCantPersona());
+    }
+}
