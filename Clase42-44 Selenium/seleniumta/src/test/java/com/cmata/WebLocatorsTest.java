d11c4b74736addfe8922ba827eaa8ee89f4d6447
samuel1509 <samuel-1509@hotmail.com>
2023-08-07 19:09:13 -0300

    Actualizacion repositorio 07-08-2023

--- Clase42-44 Selenium/seleniumta/src/test/java/com/cmata/WebLocatorsTest.java
+++ Clase42-44 Selenium/seleniumta/src/test/java/com/cmata/WebLocatorsTest.java
@@ -1,18 +1,11 @@
 package com.cmata;
 
-import java.util.concurrent.TimeUnit;
-
-import org.apache.hc.core5.util.Asserts;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 class WebLocatorsTest {
@@ -38,15 +31,20 @@ class WebLocatorsTest {
      * id="password"
      */
 
-    WebDriver driverFirefox = null;
     WebDriver dvrChrome = null;
 
     @Test
-    void testElementosByIdFFox() {
-        // WebDriverManager.fire
+    void testElementosByIdPortfolio() {
+        WebDriverManager.chromedriver().setup();
+        dvrChrome = new ChromeDriver();
+        dvrChrome.navigate().to("https://portfoliofrontend-danyalexandr.web.app/login");
+        WebElement userInput = dvrChrome.findElement(By.id("email"));
+        WebElement pwInput = dvrChrome.findElement(By.id("password"));
+        Assertions.assertTrue(userInput.isDisplayed());
+        userInput.sendKeys("usuarioPrueba");
 
-        driverFirefox = new FirefoxDriver();
-        driverFirefox.navigate().to("http://sistema.club.lagaceta.com/Forms/Default.aspx");
+        Assertions.assertTrue(pwInput.isDisplayed());
+        pwInput.sendKeys("usuarioPrueba");
 
     }
 
@@ -60,6 +58,25 @@ class WebLocatorsTest {
         Assertions.assertTrue(userInput.isDisplayed());
         userInput.sendKeys("usuarioPrueba");
 
+        Assertions.assertTrue(pwInput.isDisplayed());
+        pwInput.sendKeys("usuarioPrueba");
+
+    }
+
+    @Test
+    void testElementosByIdOpenai() {
+        WebDriverManager.chromedriver().setup();
+        dvrChrome = new ChromeDriver();
+        dvrChrome.navigate().to(
+                "https://auth0.openai.com/u/signup/identifier?state=hKFo2SAyX3ljUzJMNjdmclFMMnAzOGtjd1FoNE1mem0wWjROT6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIHo1eGxXUHp4LTRjSWRJcF9LSmowam8yRWRoRFZtZGJ4o2NpZNkgRFJpdnNubTJNdTQyVDNLT3BxZHR3QjNOWXZpSFl6d0Q");
+        WebElement userInput = dvrChrome.findElement(By.id("username"));
+
+        WebElement button = dvrChrome.findElement(By.xpath("//button[contains(text(),'Continue')]"));
+        Assertions.assertTrue(button.isDisplayed());
+        Assertions.assertTrue(userInput.isDisplayed());
+        userInput.sendKeys("usuarioPrueba@prueba.com");
+        button.click();
+        WebElement pwInput = dvrChrome.findElement(By.id("password"));
         Assertions.assertTrue(pwInput.isDisplayed());
         pwInput.sendKeys("usuarioPrueba");
 
